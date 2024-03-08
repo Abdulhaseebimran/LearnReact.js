4b66fe80a9ae0baca9db8b5a182b04fa64cb93cb
Abdulhaseebimran <haseebimran0001@gmail.com>
2024-03-08 14:49:50 +0500

    setting environment variable and some packages

---
+++ 12MegaBlogAppwrite/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  return (
+    <>
+    <h1>Hello Blog with Appwrite</h1>
+    </>
+  )
+}
+
+export default App
