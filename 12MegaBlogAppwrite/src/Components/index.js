4994c0fe483849deeed596723a906a2442f740b4
Abdulhaseebimran <haseebimran0001@gmail.com>
2024-03-14 02:00:42 +0500

    Adding form and slug values

--- 12MegaBlogAppwrite/src/Components/index.js
+++ 12MegaBlogAppwrite/src/Components/index.js
@@ -3,6 +3,7 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE"
 
 export { 
     Header, 
@@ -10,4 +11,5 @@ export {
     Container,
     Logo,
     LogoutBtn, 
+    RTE
 };
